73187cd1eb718d69397f8e89102f09e355eae73c
StopEarth <37142710+StopEarth@users.noreply.github.com>
2018-03-07 14:50:11 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._4273e10572a8d833d646446f0521a766__sprites_hd_wallpaper;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(59, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 151);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.Milka_Oreo_300g_Chocolate_jpg_350x350;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(59, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 151);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.maxresdefault;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(59, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 151);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            this.button3.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._69995fcebd9e233c74fc127cfb64421a;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(59, 520);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(225, 151);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            this.button4.MouseHover += new System.EventHandler(this.button4_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(469, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 114);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._1200px_1_hryvnia_2006_front;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(375, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(199, 97);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._2_hryvnia_2005_front;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(592, 187);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(199, 97);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._5_hryvnia_2005_front;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(811, 187);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(199, 97);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(655, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(274, 114);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "UAH";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(449, 343);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(499, 229);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.Location = new System.Drawing.Point(525, 588);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(329, 121);
+            this.button8.TabIndex = 10;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.Location = new System.Drawing.Point(974, 12);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 92);
+            this.button9.TabIndex = 11;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1090, 736);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WindowsFormsApp1.Properties;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            button1.BackgroundImage = Resources.cennik1;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackgroundImage = Resources._4273e10572a8d833d646446f0521a766__sprites_hd_wallpaper;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button2.BackgroundImage = Resources.cennik3;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackgroundImage = Resources.Milka_Oreo_300g_Chocolate_jpg_350x350;
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            button3.BackgroundImage = Resources.cennik2;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            button3.BackgroundImage = Resources.maxresdefault;
+        }
+
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+            button4.BackgroundImage = Resources.cennik4;
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button4.BackgroundImage = Resources._69995fcebd9e233c74fc127cfb64421a;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            num += 1;
+            label1.Text = Convert.ToString(num);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            num += 2;
+            label1.Text = Convert.ToString(num);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            num += 5;
+            label1.Text = Convert.ToString(num);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            if (num - 12 >= 0)
+            {
+                num -= 12;
+                label1.Text = Convert.ToString(num);
+                pictureBox1.BackgroundImage = Resources.sprite__1_;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            if (num - 15 >= 0)
+            {
+                num -= 15;
+                label1.Text = Convert.ToString(num);
+                pictureBox1.BackgroundImage = Resources._205652;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            if (num - 7 >= 0)
+            {
+                num -= 7;
+                label1.Text = Convert.ToString(num);
+                pictureBox1.BackgroundImage = Resources._43049;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(label1.Text);
+            if (num - 5 >= 0)
+            {
+                num -= 5;
+                label1.Text = Convert.ToString(num);
+                pictureBox1.BackgroundImage = Resources._1928_large;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Resources.foodbox_long;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
